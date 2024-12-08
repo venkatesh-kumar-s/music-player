bd2bd7ebf6f4d1206fda9c3879cd9be6287d4d0e
venkatesh-kumar-s <venkatesh.verto@gmail.com>
2024-12-08 21:34:09 +0530

    Updated redirect URL with netlify

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientID = "1c487413def14c3c9c2afdb6d4b9a2bc";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://music-onthego.netlify.app/"; //"http://localhost:3000/";
 const scopes = ["user-library-read", "playlist-read-private"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join(
